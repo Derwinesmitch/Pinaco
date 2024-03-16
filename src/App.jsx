a61cbee74b39f2bb2ead489ba2b4c90597fbd9aa
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2024-03-16 16:03:24 +0100

    Added Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Carousel from "./components/Carousel";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="main">
       <Header />
+      <Navbar />
       <div></div>
       <Carousel />
     </div>
