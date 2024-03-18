473d14b24d1aeb3d58c012f12c2ac57ef115ef11
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2024-03-18 19:59:27 +0100

    Reduced logo size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
       <div className="flex flex-col items-center">
         <div className="m-1 flex justify-center">
           <img
-            className="w-1/6 h-1/6"
+            className="w-1/12 h-1/12"
             src="https://scontent-ber1-1.xx.fbcdn.net/v/t39.30808-6/326696853_1170258603605040_650900985396845983_n.png?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=zKtxWgAp0ScAX_defKc&_nc_ht=scontent-ber1-1.xx&oh=00_AfCiLOJ5AU44CZ7ewymWRQLqdqhFE4HgIHDbn2_mWFiARg&oe=65FB0039"
           ></img>
         </div>
