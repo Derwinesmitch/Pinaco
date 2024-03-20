b92a42b0258a896994309a84c8ab91a936bdc697
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2024-03-20 21:16:32 +0100

    Added divs for links, added test image

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,5 +1,30 @@
+import newImage from "../assets/clases.svg";
 const Links = () => {
-  return <div>Links</div>;
+  return (
+    <div className="flex">
+      <div className="p-2">
+        <img src={newImage}></img>
+      </div>
+      <div className="p-2">
+        <img src={newImage}></img>
+      </div>
+      <div className="p-2">
+        <img src={newImage}></img>
+      </div>
+      <div className="p-2">
+        <img src={newImage}></img>
+      </div>
+      <div className="p-2">
+        <img src={newImage}></img>
+      </div>
+      <div className="p-2">
+        <img src={newImage}></img>
+      </div>
+      <div className="p-2">
+        <img src={newImage}></img>
+      </div>
+    </div>
+  );
 };
 
 export default Links;
