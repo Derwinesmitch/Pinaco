2c60d71e837880fe956307fa6af36272c8366e8e
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2024-03-20 21:18:19 +0100

    Created quotes component

---
+++ src/components/Quotes.jsx
@@ -0,0 +1,35 @@
+const Quotes = () => {
+  return (
+    <div>
+      <div className="flex justify-center">
+        <h2>Frases</h2>
+      </div>
+      <div className="flex flex-col">
+        <div className="flex justify-center">imagen</div>
+        <div className="flex justify-center">
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet.
+        </div>
+        <div>Firma</div>
+      </div>
+      <div>
+        <div>imagen</div>
+        <div>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam ero
+          sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </div>
+        <div>Firma</div>
+      </div>
+    </div>
+  );
+};
+
+export default Quotes;
