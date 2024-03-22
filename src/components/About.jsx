cdfb44403d02714d0a30f902dac741f3a47b9fb3
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2024-03-22 23:04:19 +0100

    Added css and data

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,40 @@
+import newpic from "../assets/newpic.jpg";
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="flex flex-col items-center justify-center py-8">
+      <div className="mb-8">
+        <img src={newpic} className="max-w-[400px]"></img>
+      </div>
+      <div className="text-center mb-8">
+        <h1 className="text-2xl font-bold mb-4">Acerca de Sofi 3 </h1>
+        <p>
+          Sofi es re calidad y esas cosas y etc bla bla Sofi es re calidad y
+          esas cosas y etc bla bla Sofi es re calidad y esas cosas y etc bla bla
+          Sofi es re calidad y esas cosas y etc bla blaSofi es re calidad y esas
+          cosas y etc bla blaSofi es re calidad y esas cosas y etc bla blaSofi
+          es re calidad y esas cosas y etc bla blaSofi es re calidad y esas
+          cosas y etc bla blaSofi es re calidad y esas cosas y etc bla blaSofi
+          es re calidad y esas cosas y etc bla blaSofi es re calidad y esas
+          cosas y etc bla blaSofi es re calidad y esas cosas y etc bla blaSofi
+          es re calidad y esas cosas y etc bla bla{" "}
+        </p>
+        <p>
+          Sofi es re calidad y esas cosas y etc bla blaSofi es re calidad y esas
+          cosas y etc bla blaSofi es re calidad y esas cosas y etc bla
+          blaSofiSofi es re calidad y esas cosas y etc bla blaSofi es re calidad
+          y esas cosas y etc bla blaSofi es re calidad y esas cosas y etc bla
+          blaSofi es re calidad y esas coSofi es re calidad y esas cosas y etc
+          bla blaSofi es re calidad y esas cosas y etc bla blaSofi es re calidad
+          y esas cosas y etc bla blaSofi es re calidad y esas cosas y etc bla
+          blaSofi es re calidad y esas cosas y etc bla blaSofi es re calidad y
+          esas cosas y etc bla blaSofi es re calidad y esas cosas y etc bla
+          blaSofi es re calidad y esas cosas y etc bla blasas y etc bla blaSofi
+          es re calidad y esas cosas y etc bla bla es re calidad y esas cosas y
+          etc bla blaSofi es re calidad y esas cosas y etc bla bla
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
