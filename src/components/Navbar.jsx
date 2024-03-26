f84a8bc13f2ea76f4d236626d75d36c4db9dadc0
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2024-03-26 11:54:37 +0100

    Added classes for mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="">
+    <div className="flex justify-center">
       <div className="flex flex-col items-center">
         <div className="m-1 flex justify-center">
           <img
-            className="w-1/12 h-1/12"
+            className="w-1/2 md:w-1/12 h-auto min-w-20"
             src="https://scontent-ber1-1.xx.fbcdn.net/v/t39.30808-6/326696853_1170258603605040_650900985396845983_n.png?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=zKtxWgAp0ScAX_defKc&_nc_ht=scontent-ber1-1.xx&oh=00_AfCiLOJ5AU44CZ7ewymWRQLqdqhFE4HgIHDbn2_mWFiARg&oe=65FB0039"
           ></img>
         </div>
-        <div className="flex m-1">
+        <div className="flex flex-wrap justify-center">
           <Link to="/" className="m-1">
             Home
           </Link>
